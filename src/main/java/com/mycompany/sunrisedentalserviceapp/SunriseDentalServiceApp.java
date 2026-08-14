3c17e33e65ce32d3f536204acd884d027690947a
sandali-tharu <dadimunitharu2026@gmail.com>
2026-08-14 15:30:17 +0530

    Initial commit: Base project setup with Maven

---
+++ src/main/java/com/mycompany/sunrisedentalserviceapp/SunriseDentalServiceApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sunrisedentalserviceapp;
+
+/**
+ *
+ * @author USER
+ */
+public class SunriseDentalServiceApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
