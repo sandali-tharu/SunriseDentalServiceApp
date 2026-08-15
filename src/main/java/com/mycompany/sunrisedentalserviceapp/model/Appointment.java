3b453ec2a07ce333b6e87abc9394797d56c227c3
sandali-tharu <dadimunitharu2026@gmail.com>
2026-08-15 10:26:36 +0530

    feat(model): add Appointment attributes and default constructor

---
+++ src/main/java/com/mycompany/sunrisedentalserviceapp/model/Appointment.java
@@ -0,0 +1,17 @@
+package com.mycompany.sunrisedentalserviceapp.model;
+
+public class Appointment {
+
+    private int appointmentId;
+    private String appointmentNo;
+    private int patientId;
+    private String dentistName;
+    private String treatmentType;
+    private String appointmentDate;
+    private String appointmentTime;
+    private String status;
+
+    // Default Constructor
+    public Appointment() {
+    }
+}
